6ac5526a0bdcc960a47a7babdd98a7e7c76a3915
sanuptpm <sanuptpm@gmail.com>
2016-05-09 16:30:44 +0530

    login status_code test added

--- employee_view/test/login_test.py
+++ employee_view/test/login_test.py
@@ -14,6 +14,7 @@ print response.headers
 
 print response.request.headers
 
+
 #Enployee list request
 url =  "http://127.0.0.1:8000/employee_list/"
 response = session.get(url)
@@ -23,3 +24,9 @@ response = session.get(url)
 print response.headers
 
 print response.request.headers
+
+req_status = requests.get('http://127.0.0.1:8000/employee_list/')
+print "...login status_code .....", req_status.status_code
+
+
+
