e92c5061c138b09a24422f460a17b5de92e09bc1
sanuptpm <sanuptpm@gmail.com>
2016-05-10 22:12:21 +0530

    status_code test update added

--- employee_view/backends.py
+++ employee_view/backends.py
@@ -5,7 +5,6 @@ class ProfileAuthBackend(object):
 	def authenticate(self, imei=None, mac=None):
 		try:
 			user = Profile.objects.get(imei=imei, mac=mac)
-			print ".authenticate...user", user
 			return user
 		except Profile.DoesNotExist:
 			return None
--- employee_view/test/login_test.py
+++ employee_view/test/login_test.py
@@ -6,27 +6,46 @@ session = requests.Session()
 
 #Login request
 data = {"imei":"124", "mac":"986"}
+
+
+
 response = session.post(url, data=data)
+
+print ".............login json response.........."
 
 print response.json()
 
+print ".............login headers response.........."
+
 print response.headers
+
+print ".............login response.request.headers.........."
 
 print response.request.headers
 
+login_status = requests.get(url,data=data)
+
+print "...login status_code....", login_status.status_code
 
 #Enployee list request
 url =  "http://127.0.0.1:8000/employee_list/"
+
 response = session.get(url)
+
+#print ".............employee_list json response.........."
 
 #print response.json()
 
+print ".............employee_list response.headers.........."
+
 print response.headers
+
+print ".............employee_list response.request.headers.........."
 
 print response.request.headers
 
-req_status = requests.get('http://127.0.0.1:8000/employee_list/')
-print "...login status_code .....", req_status.status_code
+emp_status = requests.get(url)
 
+print "...emp status code....", emp_status.status_code
 
-
+#print "...login.....", emp_status.text
