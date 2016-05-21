3f87706e1eb999247d87517e94c681091aee3412
Saju Madhavan <sajuptpm@gmail.com>
2016-05-22 03:28:32 +0530

    indentation fix

--- employee/test/load_data.py
+++ employee/test/load_data.py
@@ -1,23 +1,21 @@
 from employee.models import Profile 
 from django.db.models import Q
 
-
 def profile_data(imei,mac):
-	data = Profile.objects.filter(Q(imei=imei) | Q(mac=mac))
-	if not data:
-		print "---------DATA  %d %d SAVED-----------------" %(imei, mac)
-		data = Profile(imei=imei, mac=mac)
-		data.save()
-	else:
-		print "--------USER %d %d ALREADY EXISTED----------" %(imei, mac)
-# DATA FOR PROFILE
+    data = Profile.objects.filter(Q(imei=imei) | Q(mac=mac))
+    if not data:
+        print "---------DATA  %d %d SAVED-----------------" %(imei, mac)
+        data = Profile(imei=imei, mac=mac)
+        data.save()
+    else:
+        print "--------USER %d %d ALREADY EXISTED----------" %(imei, mac)
 
+# DATA FOR PROFILE
 data = [(1,2),(2,2),(3,3)]
 
 for x in data:
-	print ""
-	profile_data(x[0], x[1])
-
+    print ""
+    profile_data(x[0], x[1])
 
 
 
