836c513082ae933a26c4f9a074faad691dc7266c
Saju Madhavan <sajuptpm@gmail.com>
2016-05-21 21:40:53 +0530

    updated login test script to add base url

--- employee/test/login_test.py
+++ employee/test/login_test.py
@@ -1,10 +1,12 @@
 import requests
 
+BASE_URL="http://127.0.0.1:8000/"
+
 class LoginCheck:
 	def __init__(self):
 		self.session = requests.Session()
-	def check_login_success(self,imei,mac):
-		url = "http://127.0.0.1:8000/login/"
+	def check_login(self,imei,mac):
+		url = BASE_URL+"login/"
 		data = {"imei":imei, "mac":mac}
 		response = self.session.post(url, data=data)
 		#print ".............login json response.........."
@@ -14,8 +16,8 @@ class LoginCheck:
 		#print ".............login response.request.headers.........."
 		#print response.request.headers
 		print "...LOGIN STATUS CODE....", response.status_code
-	def check_employee_success(self):
-		url =  "http://127.0.0.1:8000/employees/"
+	def check_employee_list(self):
+		url =  BASE_URL+"employees/"
 		response = self.session.get(url)
 		#print ".............employee_list json response.........."
 		#print response.json()
@@ -25,12 +27,16 @@ class LoginCheck:
 		#print response.request.headers
 		print "...EMP STATUS CODE....", response.status_code
 
-print "-------SUCCESS LOGIN-----------"
+print "-------CHECKING SUCCESS LOGIN-----------"
 obj = LoginCheck()
-obj.check_login_success(124,986)
-obj.check_employee_success()
-print "--------FAILED LOGIN-----------"
+obj.check_login(124,986)
+obj.check_employee_list()
+
+print "--------CHECKING FAILED LOGIN-----------"
 obj = LoginCheck()
-obj.check_login_success(14,86)
-obj.check_employee_success()
+obj.check_login(14,86)
+obj.check_employee_list()
+
+
+
 
