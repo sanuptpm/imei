5426601e748fa4a0ccc6a824a400bff07a3466a6
Saju Madhavan <sajuptpm@gmail.com>
2016-05-22 03:24:57 +0530

    fixed intendation

--- employee/test/login_test.py
+++ employee/test/login_test.py
@@ -3,33 +3,33 @@ import requests
 BASE_URL="http://127.0.0.1:8000/"
 
 class LoginCheck:
-	def __init__(self):
-		self.session = requests.Session()
-	def check_login(self,imei,mac):
-		url = BASE_URL+"login"
-		data = {"imei":imei, "mac":mac}
-		response = self.session.post(url, data=data)
-		#print ".............login json response.........."
-		#print response.json()
-		#print "...............content of the server's response..............."
-		#print response.text
-		#print ".............login headers response.........."
-		#print response.headers
-		#print ".............login response.request.headers.........."
-		#print response.request.headers
-		print "...LOGIN STATUS CODE....", response.status_code
-	def check_employee_list(self):
-		url =  BASE_URL+"employees"
-		response = self.session.get(url)
-		#print ".............employee_list json response.........."
-		#print response.json()
-		#print "...............content of the server's response..............."
-		#print response.text
-		#print ".............employee_list response.headers.........."
-		#print response.headers
-		#print ".............employee_list response.request.headers.........."
-		#print response.request.headers
-		print "...EMP STATUS CODE....", response.status_code
+    def __init__(self):
+        self.session = requests.Session()
+    def check_login(self,imei,mac):
+        url = BASE_URL+"login"
+        data = {"imei":imei, "mac":mac}
+        response = self.session.post(url, data=data)
+        #print ".............login json response.........."
+        #print response.json()
+        #print "...............content of the server's response..............."
+        #print response.text
+        #print ".............login headers response.........."
+        #print response.headers
+        #print ".............login response.request.headers.........."
+        #print response.request.headers
+        print "...LOGIN STATUS CODE....", response.status_code
+    def check_employee_list(self):
+        url =  BASE_URL+"employees"
+        response = self.session.get(url)
+        #print ".............employee_list json response.........."
+        #print response.json()
+        #print "...............content of the server's response..............."
+        #print response.text
+        #print ".............employee_list response.headers.........."
+        #print response.headers
+        #print ".............employee_list response.request.headers.........."
+        #print response.request.headers
+        print "...EMP STATUS CODE....", response.status_code
 
 print "-------CHECKING SUCCESS LOGIN-----------"
 obj = LoginCheck()
