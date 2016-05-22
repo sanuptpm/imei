99ad209acf54e3e97def65080dfeedb903b936dd
sanuptpm <sanuptpm@gmail.com>
2016-05-22 14:04:06 +0530

    urls updated

--- empl/urls.py
+++ empl/urls.py
@@ -14,10 +14,8 @@ Including another URLconf
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
 from django.conf.urls import url, include
-from django.contrib import admin
 import employee
 
 urlpatterns = [
-    url(r'^admin/', admin.site.urls),
     url(r'^', include('employee.urls')),
 ]
--- employee/urls.py
+++ employee/urls.py
@@ -1,12 +1,11 @@
 from django.conf.urls import patterns, include, url
-from django.contrib import admin
 from django.conf import settings
 
 urlpatterns = patterns('',
 
-	url(r'^employee', 'employee.views.add_employee', name='employee_form'),
-	url(r'^employees', 'employee.views.list_employee', name='employee_list'),
-	url(r'^login', 'employee.views.login_view', name='login'),
+	url(r'^employee$', 'employee.views.add_employee', name='employee_form'),
+	url(r'^employees$', 'employee.views.list_employee', name='employee_list'),
+	url(r'^login$', 'employee.views.login_view', name='login'),
 	
 	
 )
