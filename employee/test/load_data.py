f0d74a9c6643a3f6fb920bc3a031d716d80f01c9
sanuptpm <sanuptpm@gmail.com>
2016-06-02 23:07:44 +0530

    load data input updated

--- employee/test/load_data.py
+++ employee/test/load_data.py
@@ -11,7 +11,7 @@ def profile_data(imei,mac):
         print "--------USER %s %s ALREADY EXISTED----------" %(imei, mac)
 
 # DATA FOR PROFILE
-data = [(1,2),(2,2),('352159066842202','c4:50:06:06:c1:d3')]
+data = [('353043059350234','1c:b0:94:d4:fc:47'),('353327061453619','f8:cf:c5:94:de:ea'),('352159066842202','c4:50:06:06:c1:d3')]
 
 for x in data:
     print ""
