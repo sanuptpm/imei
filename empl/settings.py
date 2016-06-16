20da7d60bbffac275228a6273da1692ce156d581
sanuptpm <sanuptpm@gmail.com>
2016-06-16 20:37:53 +0530

    log file location changed

--- empl/settings.py
+++ empl/settings.py
@@ -139,7 +139,7 @@ LOGGING = {
         'file': {
             'level': 'DEBUG',
             'class': 'logging.FileHandler',
-            'filename': 'employee/log/debug.log',
+            'filename': 'debug.log',
             'formatter': 'details',
         },
     },
